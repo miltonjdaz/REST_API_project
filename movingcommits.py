f513a126050a6efb3e1c412d7c40a76651b29212
miltonjdaz <miltonjdaz@yahoo.com>
2020-05-06 15:13:05 -0500

    Small modifications

--- movingcommits.py
+++ movingcommits.py
@@ -3,14 +3,12 @@ import pandas as pd
 from github import Github
 import boto3
 
-# do pip install PyGithub tomorrow 
-
 transfer = github.PullRequest.PullRequest.get_commits() 
 
-df1 = pd.DataFrame(query_one) 
+df1 = pd.DataFrame(transfer) 
   
 # saving the dataframe 
 df1.to_csv('/home/milton/github/REST_API_project/displaycommits.csv')
 
 s3 = boto3.resource('s3')
-s3.meta.client.upload_file(file_name, 'YOUR_S3_BUCKET_NAME', 'DESIRED_S3_OBJECT_NAME')
+s3.meta.client.upload_file(displaycommits.csv, 'YOUR_S3_BUCKET_NAME', 'DESIRED_S3_OBJECT_NAME')
