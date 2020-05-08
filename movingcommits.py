148c756828feb8e6bb384c6c596017e99163547e
miltonjdaz <miltonjdaz@yahoo.com>
2020-05-08 09:55:35 -0500

    Still stuck

--- movingcommits.py
+++ movingcommits.py
@@ -19,3 +19,5 @@ df1.to_csv('/home/milton/github/REST_API_project/displaycommits.csv')
 s3 = boto3.resource('s3')
 s3.meta.client.upload_file(displaycommits.csv, 'YOUR_S3_BUCKET_NAME', 'DESIRED_S3_OBJECT_NAME')
 """
+
+import ipdb; ipdb.set_trace()
