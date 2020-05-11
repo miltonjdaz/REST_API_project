7290aa01a502a4154db78bd41f342e287b288c6d
miltonjdaz <miltonjdaz@yahoo.com>
2020-05-11 15:54:17 -0500

    Still stuck

--- movingcommits.py
+++ movingcommits.py
@@ -1,8 +1,11 @@
 # Project number 3 
+
 import pandas as pd 
 from github import Github 
 "import boto3"
 import os
+
+# Protecting my username and password 
 
 gh_us = os.environ.get('gh_user')
 gh_pw = os.environ.get('gh_pass')
