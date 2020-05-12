1a71dbf067ac65e8beb6d6079741f24141c66e2a
miltonjdaz <miltonjdaz@yahoo.com>
2020-05-12 10:59:43 -0500

    Small changes

--- movingcommits.py
+++ movingcommits.py
@@ -10,14 +10,20 @@ import os
 gh_us = os.environ.get('gh_user')
 gh_pw = os.environ.get('gh_pass')
 
+# For some reason, this is not working
+
 github = Github(gh_us, gh_pw)
 transfer = github.PullRequest.PullRequest.get_commits() 
 
+# Code to make have transfer become a dataframe
+
 df1 = pd.DataFrame(transfer) 
   
-# saving the dataframe 
+# Saving the dataframe into a CSV file 
 df1.to_csv('/home/milton/github/REST_API_project/displaycommits.csv')
 
+
+# This is on hold as I try to fix the github pull request 
 """
 s3 = boto3.resource('s3')
 s3.meta.client.upload_file(displaycommits.csv, 'YOUR_S3_BUCKET_NAME', 'DESIRED_S3_OBJECT_NAME')
